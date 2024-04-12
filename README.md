f3aae37f8a418842dbc64b2e7ad6cc3f0457da88
Christopher Chen <chrischen0127@gmail.com>
2024-04-12 17:01:05 +0800

    Move instructions for practice set to index.js, add README content

---
+++ README.md
@@ -0,0 +1,6 @@
+# React useState Hook Practice Set
+
+## Instructions
+Hooks are crucial to the dynamic nature of React programming.
+This is a simple exercise designed for you to get some practice using useState hooks in React.
+Refer to Parts 1 & 2 in __index.js__ for detailed instructions on how to complete this practice set.
--- react-hook-practice/src/index.js
+++ react-hook-practice/src/index.js
@@ -3,3 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// Part 1
+// Given that we can fetch and log the current time using the snippet below:
+//   let time = new Date().toLocaleTimeString();
+//   console.log(time);
+// you will update the code in the App component to show the latest time in the <h1> whenever the Get Time button is pressed.
+
+
+// Part 2 (extra credit)
+// 2. As an extra challenge, get the time in your <h1> to update every second, such as to mimic the function of a real clock!
+
+// Hint: you can get code to be called every second using the setInterval method.
